804072bb094d655855c3424db8a4861e830d24d7
gaieepo <gaieepo@gmail.com>
2016-06-18 18:42:36 +0800

    retrieve json using pixabay

--- src/com/example/PhotoGallery/FlickrFetchr.java
+++ src/com/example/PhotoGallery/FlickrFetchr.java
@@ -1,5 +1,8 @@
 package com.example.PhotoGallery;
 
+import android.net.Uri;
+import android.util.Log;
+
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +13,11 @@ import java.net.URL;
  * Created by gaieepo on 17/6/2016.
  */
 public class FlickrFetchr {
+    public static final String TAG = "FlickrFetchr";
+
+    private static final String ENDPOINT = "https://pixabay.com/api/";
+    private static final String API_KEY = "2773707-6b9eab3a9f01c7d3e40fa9a59";
+
     byte[] getUrlBytes(String urlSpec) throws IOException {
         URL url = new URL(urlSpec);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -36,5 +44,21 @@ public class FlickrFetchr {
 
     public String getUrl(String urlSpec) throws IOException {
         return new String(getUrlBytes(urlSpec));
+    }
+
+    public void fetchItems() {
+        try {
+            String url = Uri.parse(ENDPOINT).buildUpon()
+                    .appendQueryParameter("key", API_KEY)
+                    .appendQueryParameter("q", "yellow+flowers")
+                    .appendQueryParameter("image_type", "photo")
+                    .appendQueryParameter("pretty", "true")
+                    .build().toString();
+            Log.d(TAG, url);
+            String xmlString = getUrl(url);
+            Log.i(TAG, "Received xml: " + xmlString);
+        } catch (IOException ioe) {
+            Log.e(TAG, "Failed to fetch items", ioe);
+        }
     }
 }
